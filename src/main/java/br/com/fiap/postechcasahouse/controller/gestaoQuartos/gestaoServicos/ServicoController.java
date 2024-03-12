13b11237eb5a90646855ffd646191a76b253c7be
Bruno Miguel Morais <bm.morais21@gmail.com>
2024-03-12 19:53:48 -0300

    Buscando os serviços cadastrados

--- src/main/java/br/com/fiap/postechcasahouse/controller/gestaoQuartos/gestaoServicos/ServicoController.java
+++ src/main/java/br/com/fiap/postechcasahouse/controller/gestaoQuartos/gestaoServicos/ServicoController.java
@@ -1,17 +1,17 @@
 package br.com.fiap.postechcasahouse.controller.gestaoQuartos.gestaoServicos;
 
+import br.com.fiap.postechcasahouse.DTO.gestaoQuartos.LocalidadeDTO;
 import br.com.fiap.postechcasahouse.DTO.gestaoServicos.ServicoDTO;
 import br.com.fiap.postechcasahouse.service.gestaoServicos.ServicoService;
 
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/servicos")
@@ -19,6 +19,16 @@ public class ServicoController {
 
     @Autowired
     private ServicoService servicoService;
+
+    @GetMapping
+    public ResponseEntity<Page<ServicoDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<ServicoDTO> addresses = servicoService.findAll(pageRequest);
+        return ResponseEntity.ok().body(addresses);
+    }
 
     @PostMapping
     public ResponseEntity<ServicoDTO> save(@RequestBody @Valid ServicoDTO servicoDTO) {
--- src/main/java/br/com/fiap/postechcasahouse/service/gestaoServicos/ServicoService.java
+++ src/main/java/br/com/fiap/postechcasahouse/service/gestaoServicos/ServicoService.java
@@ -1,10 +1,14 @@
 package br.com.fiap.postechcasahouse.service.gestaoServicos;
 
+import br.com.fiap.postechcasahouse.DTO.gestaoQuartos.LocalidadeDTO;
 import br.com.fiap.postechcasahouse.DTO.gestaoServicos.ServicoDTO;
+import br.com.fiap.postechcasahouse.entity.gestaoQuartos.Localidade;
 import br.com.fiap.postechcasahouse.entity.gestaoServicos.Servico;
 import br.com.fiap.postechcasahouse.repository.gestaoServicos.ServicoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +17,11 @@ public class ServicoService {
 
     @Autowired
     private ServicoRepository servicoRepository;
+
+    public Page<ServicoDTO> findAll(PageRequest pageRequest) {
+        Page<Servico> localidades = servicoRepository.findAll(pageRequest);
+        return localidades.map(ServicoDTO::new);
+    }
 
     public ServicoDTO save(ServicoDTO servicoDTO) {
         Servico servico = new Servico();
